bf55ed4b1b5336bfc12aa1590fc0feca1615c34e
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-08 01:44:34 +0800

    Push to github -initial commit

---
+++ String.js
@@ -0,0 +1,62 @@
+//String 
+const firstName = 'Jose';
+const lastName = 'Miguel';
+const age = '19';
+const str = 'Hello my name is Walter White';
+const tags = 'Web Development, Web Design, Progamming';
+const quote = " Sky is the limit."
+
+//Concatenation
+let val; 
+val = firstName +" "+ lastName;
+console.log(val);
+
+//Append
+dog = "Naruto ";
+dog += "Uzumaki";
+console.log(dog);
+
+cat = "My name is " + firstName +" " + lastName + " and is" + age;
+console.log(cat); 
+
+//Escaping
+Escape = "You're you and I'm me";
+console.log(Escape);
+
+//Length
+pogi = firstName.length;
+console.log(meth);
+
+//concat
+hotdog = firstName.concat(' ' + lastName);
+console.log(hotdog);
+
+//Change case
+me = firstName.toLowerCase()+ " ";
+me += lastName.toUpperCase();
+console.log(me);
+
+val = firstName[3];
+lav = firstName.charAt('3')
+console.log(lav);
+
+//Substring
+jenz = firstName.substring(3, 7);
+console.log(jesse);
+
+//Slice
+zel = firstName.slice(0,4);
+negative = firstName.slice(-6);
+console.log(negative);
+
+//split
+val = quote.split(" ");
+console.log(val);
+
+//Replace
+palit = str.replace('Walter White', 'Jose Miguel');
+console.log(change);
+
+//Inlcludes
+kasali = quote.includes('mind');
+console.log(included);
